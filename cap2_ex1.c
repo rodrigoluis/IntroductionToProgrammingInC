0ec3707819cfbe8f0eb704e4a9bef6fc5b7d3ea2
Rodrigo <rodrigoluis@gmail.com>
2021-05-20 17:12:05 -0300

    Add codes

---
+++ cap2_ex1.c
@@ -0,0 +1,18 @@
+/* Fazer um programa para imprimir o seu nome, na linha seguinte
+sua idade e na terceira sua altura.
+
+Make a program to print your name, in the line following your
+age and in the third your height.
+*/
+
+#include <stdio.h>
+#include <stdbool.h>
+
+
+int main()
+{
+    printf("Rodrigo Luis\n");
+    printf("42\n");
+    printf("1.82\n");
+    return 0;
+}
