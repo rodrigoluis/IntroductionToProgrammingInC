58e3606a493e25e070a3f22d57c12555a45f4db9
rodrigoluis <rodrigoluis@gmail.com>
2021-05-23 11:19:31 -0300

    adding new codes

---
+++ cap3/cap3_ex2.c
@@ -0,0 +1,23 @@
+/*
+Write a function to receive as parameter the duration of an 
+experiment expressed in seconds and print that time in hours, 
+minutes and seconds.
+*/
+
+#include <stdio.h>
+
+void duration(int sec)
+{
+    int hours, minutes, seconds;
+    hours = sec / 3600;
+    minutes = (sec % 3600) / 60;
+    seconds = (sec % 3600) % 60;
+    printf("%02d:%02d:%02d\n", hours,minutes,seconds);        
+}
+
+int main()
+{
+    // Testing the function
+    duration(10925); // Expected 03:02:03
+    return 0;
+}
